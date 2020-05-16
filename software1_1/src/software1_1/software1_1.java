4388752686c24a64432d324c2ee928caa0c0ef0e
megu <megu@localhost>
2020-05-16 20:34:12 +0900

    テスト

---
+++ software1_1/src/software1_1/software1_1.java
@@ -0,0 +1,10 @@
+package software1_1;
+
+public class software1_1 {
+	
+	public static void main(String[] args) {
+		System.out.println("好きな食べ物：ハンバーグ");
+		System.out.println("理由：外はカリッと中はジューシーで美味しいから。");
+	}
+
+}
